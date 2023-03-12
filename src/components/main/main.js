e7f3cbbf43ea396bb4ab59c5687451f21a0bb0f9
LosevaMarina <104833079+LosevaMarina@users.noreply.github.com>
2023-03-12 21:53:28 +0300

    main

---
+++ src/components/main/main.js
@@ -0,0 +1,15 @@
+import BurgerIngredients from '../burger-ingredients/burger-ingredients';
+import BurgerConstructor from '../burger-constructor/burger-constructor';
+import styles from './main.module.css';
+
+
+function Main() {
+  return (
+    <main className={ styles.main }>
+      {/*<BurgerIngredients />
+      <BurgerConstructor />*/}
+    </main>
+  );
+}
+
+export default Main;
