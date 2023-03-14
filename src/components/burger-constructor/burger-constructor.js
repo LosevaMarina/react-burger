47fbdaa0e1ff907cc91125499556132a0060ab6f
LosevaMarina <104833079+LosevaMarina@users.noreply.github.com>
2023-03-14 03:42:55 +0300

    BurgerConstructor

--- src/components/burger-constructor/burger-constructor.js
+++ src/components/burger-constructor/burger-constructor.js
@@ -1,33 +1,49 @@
 import React from 'react';
-import { ConstructorElement } from '@ya.praktikum/react-developer-burger-ui-components';
-
+import { ConstructorElement, CurrencyIcon, Button } from '@ya.praktikum/react-developer-burger-ui-components';
+import data from '../utils/data';
 import styles from './burger-constructor.module.css';
 
 function BurgerConstructor () {
+  console.log(data.length);
         return (
-            <div className={styles.block}>
-          <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+          <div className={styles.block}>
+            <div className={styles.element}>
+                <ConstructorElement
+                  type="top"
+                  isLocked={true}
+                  text={data[0].name}
+                  price={data[0].price}
+                  thumbnail={data[0].image}
+                />            
+            </div>
+            <ul className={styles.list}>
+              {data.map((obj) => (
+                <li key={obj._id} className={styles.listItem}>
+                  <div className={styles.points}></div>
+                  <ConstructorElement
+                    text={obj.name}
+                    price={obj.price}
+                    thumbnail={obj.image}
+                  />     
+                </li>         
+              ))}
+            </ul>
+            <div className={styles.element}>
             <ConstructorElement
-              type="top"
-              isLocked={true}
-              text="Краторная булка N-200i (верх)"
-              price={200}
-
-            />
-            <ConstructorElement
-              text="Краторная булка N-200i (верх)"
-              price={50}
-
-            />
-            <ConstructorElement
-              type="bottom"
-              isLocked={true}
-              text="Краторная булка N-200i (низ)"
-              price={200}
-              
-            />
-          </div>
-
+                  type="bottom"
+                  isLocked={true}
+                  text={data[0].name}
+                  price={data[0].price}
+                  thumbnail={data[0].image}
+                /> 
+            </div>
+            <div className={styles.sum}>
+              <p className="text text_type_digits-medium">610</p>
+              <CurrencyIcon type="primary" />
+              <Button htmlType="button" type="primary" size="large">
+                Оформить заказ
+              </Button>
+            </div>
           </div>
         )
       }
