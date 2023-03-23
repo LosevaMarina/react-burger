8382d1624f045dfb9c791f29886fe0e848dd3eb2
LosevaMarina <104833079+LosevaMarina@users.noreply.github.com>
2023-03-24 00:02:37 +0300

    ModalOverlay import

--- src/components/modal-overlay/modal-overlay.js
+++ src/components/modal-overlay/modal-overlay.js
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import styles from "../modal-overlay/modal-overlay";
+import styles from "../modal-overlay/modal-overlay.module.css";
 
 
 const ModalOverlay = (props) => {
